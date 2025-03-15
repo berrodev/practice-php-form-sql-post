ba9af66e21e5f899d00abbc209186128c31eebd6
Francisco Berroeta <fsberroeta@gmail.com>
2025-03-15 12:03:50 -0300

    Remove list.php and add insert.php file

--- index.php
+++ index.php
@@ -8,7 +8,17 @@
 </head>
 
 <body>
-
+    <main>
+        <form action="insert.php" method="post">
+            <label for="name">Nombre:</label>
+            <input type="text" name="name" id="name" required>
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email" required>
+            <label for="phone">Teléfono:</label>
+            <input type="tel" name="phone" id="phone" required>
+            <input type="submit" value="Enviar">
+        </form>
+    </main>
 </body>
 
 </html>
