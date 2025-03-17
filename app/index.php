55a4ff3323debc64ca6d0edd116d5931d26edd08
Francisco Berroeta <fsberroeta@gmail.com>
2025-03-16 22:39:09 -0300

    Add form for user input and display greeting message

--- app/index.php
+++ app/index.php
@@ -1,3 +1,17 @@
 <?php
-phpinfo();
+
+if($_POST){
+    echo "Hola mundo";
+}
+
 ?>
+
+<form method="POST">
+    <input type="text" name="name" placeholder="Nombre" required>
+    <input type="text" name="rut" placeholder="RUT" required>
+    <select name="gender">
+        <option value="M">Masculino</option>
+        <option value="F">Femenino</option>
+    </select>
+    <button type="submit">Enviar</button>
+</form>
